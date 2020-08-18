9d3436a3aa96d5cce95751536efc074339d3a907
MatStat <powdersteamboat@gmail.com>
2020-08-17 21:04:50 -0600

    Debugging

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 const util = require ("util");
 const mysql = require ("mysql");
-const connection = mysql.createConnection({
-    host: "d6q8diwwdmy5c9k9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", 
-    user: "j1sp7lovcw4ay3vf", 
-    port: 3306, 
-    password: "db3x4fu8l42rmpk1", 
-    database: "carlq82wmbo9t3h6"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        port: 3306,
+        password: "root",
+        database: "burgers_db"
+    })
+};
 
 connection.connect();
 
