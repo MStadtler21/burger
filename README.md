23d8e77b9390c0b9afd4d44cd113f3cb451e6e6c
MatStat <powdersteamboat@gmail.com>
2020-08-25 17:45:44 -0600

    Created Delete Function

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# burger
+# Node Express Handlebars: "Burger-Time"
+
+## Description
+An application that lets you submit, 'eat', and delete burgers. You can enter
--- models/burger.js
+++ models/burger.js
@@ -18,6 +18,11 @@ var burger = {
     orm.update("burgers", {devoured:true}, condition, function(res) {
       cb(res);
     });
+  },
+  delete: function(condition, cb) {
+    orm.delete("burgers", condition, function(res) {
+      cb(res);
+    });
   }
 };
 
